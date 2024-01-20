a5445c6811b227372167c7ba781bad054e52e61e
THEMADMAXOP <156558024+THEMADMAXOP@users.noreply.github.com>
2024-01-20 16:25:55 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -34,5 +34,8 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
+        [  
+            InlineKeyboardButton(text=_["S_B_5"],callback_data="gib_source"),
+        ],
     ]
     return buttons
